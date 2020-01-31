2fcf9f972dd37151e1eb9d8cc737c1197e470b56
Sean Jones <sean.jones@nextthought.com>
2020-01-30 22:33:26 -0600

    Pin redis

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'redis < 3.4.0, >= 3.3.0',
     'celery[redis,sqlalchemy]',
     'zope.interface',
     'zope.component',
